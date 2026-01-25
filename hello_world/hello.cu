64d7a8471a8678acbacafa13badd37aa6fd8e023
Corin131 <aront.19930131@gmail.com>
2026-01-25 23:40:38 +0800

    hello.cu code linted

--- hello_world/hello.cu
+++ hello_world/hello.cu
@@ -3,25 +3,25 @@
 #include <iostream>
 
 __global__ void HelloCuda() {
-    int idx = threadIdx.x;
-    printf("Hello World From GPU thread%d.\n", idx);
-
+  int idx = threadIdx.x;
+  printf("Hello World From GPU thread%d.\n", idx);
 }
 
 int main() {
-    std::cout << "Hello World From CPU." << std::endl;
-    std::cout << "=============================" << std::endl;
+  std::cout << "Hello World From CPU." << std::endl;
+  std::cout << "=============================" << std::endl;
 
-    HelloCuda<<<1, 10>>>();
-    cudaDeviceSynchronize();
+  HelloCuda<<<1, 10>>>();
+  cudaDeviceSynchronize();
 
-    if (cudaGetLastError() != cudaSuccess) {
-        std::cerr << "CUDA err:  " << cudaGetErrorString(cudaGetLastError()) << std::endl;
-        return 1;
-    }
-    std::cout << "=============================" << std::endl;
-    std::cout << "GPU: Hello CUDA finished!" << std::endl;
-    std::cout << "CPU: Hello CUDA finished!" << std::endl;
+  if (cudaGetLastError() != cudaSuccess) {
+    std::cerr << "CUDA err:  " << cudaGetErrorString(cudaGetLastError())
+              << std::endl;
+    return 1;
+  }
+  std::cout << "=============================" << std::endl;
+  std::cout << "GPU: Hello CUDA finished!" << std::endl;
+  std::cout << "CPU: Hello CUDA finished!" << std::endl;
 
-    return 0;
+  return 0;
 }
