ecf4f73ecd1cbc2171a570acd14c2b1c74c2370f
Corin131 <aront.19930131@gmail.com>
2026-01-28 18:26:36 +0800

    softmax cpu version2.

--- softmax/softmax.cu
+++ softmax/softmax.cu
@@ -1,29 +1,30 @@
 #include <cuda_runtime.h>
 
-#include <cstdio>
+#include <cmath>
+#include <cstdint>
 
 __global__ void Softmax() {}
 
 void SoftmaxCpu(const float* in, float* out, const size_t n, const size_t c) {
   // n * c datas
-  for (int i = 0; i < n; ++i) {
+  for (size_t i = 0; i < n; ++i) {
     const float* row_in = in + c * i;
     float* row_out = out + c * i;
     // get max value
-    float max_val = *row_in;
-    for (int j = 1; j < c; ++j) {
-      max_val = max(max_val, row_in[j]);
+    float max_val = row_in[0];
+    for (size_t j = 1; j < c; ++j) {
+      max_val = std::fmax(max_val, row_in[j]);
     }
 
     float sum = 0.0f;
-    for (int k = 0; k < c; ++k) {
-      row_out[k] = exp(row_in[k] - max_val);
+    for (size_t k = 0; k < c; ++k) {
+      row_out[k] = std::exp(row_in[k] - max_val);
       sum += row_out[k];
     }
     sum = 1.0f / sum;
 
     // out
-    for (int t = 0; t < c; ++t) {
+    for (size_t t = 0; t < c; ++t) {
       row_out[t] *= sum;
     }
   }
